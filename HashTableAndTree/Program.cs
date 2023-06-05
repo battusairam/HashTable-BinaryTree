c88bb2c0096834b35ac65cb6bcb64c823cdd1f54
“karandepale” <karandepale111@gmail.com>
2023-06-05 09:53:38 +0530

    Add: Logic for calculate the frequency of each word in the sentence

--- HashTableAndTree/Program.cs
+++ HashTableAndTree/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HashTableAndTree
 {
@@ -10,7 +7,37 @@ namespace HashTableAndTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash Table and Tree Programmm.....");
+            Console.WriteLine("Welcome to Hash Table and Tree Program...");
+
+            string sentence = "To be or not to be";
+            Dictionary<string, int> wordFrequency = GetWordFrequency(sentence);
+
+            Console.WriteLine("Word Frequency:");
+            foreach (var data in wordFrequency)
+            {
+                Console.WriteLine($"{data.Key}: {data.Value}");
+            }
+        }
+
+        static Dictionary<string, int> GetWordFrequency(string sentence)
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            string[] words = sentence.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    wordFrequency[word]++;
+                }
+                else
+                {
+                    wordFrequency[word] = 1;
+                }
+            }
+
+            return wordFrequency;
         }
     }
 }
