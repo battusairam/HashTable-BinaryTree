0e9e412b24020309fe8d7db5c1323cad9d65673a
“karandepale” <karandepale111@gmail.com>
2023-06-05 09:49:40 +0530

    Add:Hashtable and Tree Assignment

---
+++ HashTableAndTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table and Tree Programmm.....");
+        }
+    }
+}
